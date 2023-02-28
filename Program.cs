f29a49f004cf4fd727ec30c024ffa9307a6c44ef
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-02-28 16:06:00 +0300

    Начало алгоритма

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿void PrintArrayToConsole(int[] printArray)
+{
+    Console.Write("[");
+    for (int i = 0; i < printArray.Length; i++)
+    {
+    	if (i == printArray.Length - 1)
+        	Console.Write($"{printArray[i]}");
+        else
+        	Console.Write($"{printArray[i]}, ");
+    }
+    Console.Write("]");
+
+}
+
+int[] oldArray = { 6, 4, 8, 3, 9, 1, 2, 5, 7 };
+Console.WriteLine("Изначальный массив:");
+PrintArrayToConsole(oldArray);

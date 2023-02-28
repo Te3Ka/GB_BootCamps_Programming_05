c2d2fcebcb06cd6d0da7362112b126738e64b40a
Te3Ka_PaynE <Mnement4813@yandex.ru>
2023-02-28 16:15:59 +0300

    Выполнено

--- Program.cs
+++ Program.cs
@@ -12,6 +12,31 @@
 
 }
 
+int[] sortArrayBubble(int[] sortArray)
+{
+	int size = sortArray.Length;
+
+	for (int current = 0; current < size - 1; current++)
+	{
+		for (int i = 0; i < size - 1; i++)
+		{
+			if (sortArray[i] > sortArray[i + 1])
+			{
+				int temp = sortArray[i];
+				sortArray[i] = sortArray[i + 1];
+				sortArray[i + 1] = temp;
+			}
+		}
+		PrintArrayToConsole(sortArray);
+		Console.WriteLine($"<<< {current} итерация");
+	}
+	return sortArray;
+}
+
 int[] oldArray = { 6, 4, 8, 3, 9, 1, 2, 5, 7 };
 Console.WriteLine("Изначальный массив:");
 PrintArrayToConsole(oldArray);
+Console.WriteLine();
+int[] sortBubleArray = sortArrayBubble(oldArray);
+Console.WriteLine("Отсортированный массив:");
+PrintArrayToConsole(sortBubleArray);
